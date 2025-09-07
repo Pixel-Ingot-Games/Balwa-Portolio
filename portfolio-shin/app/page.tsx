3efd0e08fe26d21893f6d8a6597a99331b8423a4
imhassan12 <69629449+iamhassan1@users.noreply.github.com>
2025-09-07 17:20:12 +0500

    Mneu Test 1

--- portfolio-shin/app/page.tsx
+++ portfolio-shin/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="w-full h-screen" style={{ paddingLeft: '320px' }}>
+    <div className="w-full h-screen" style={{ paddingLeft: '384px' }}>
       {/* Main content area - adjusted for left-side menu */}
       <div className="flex items-center justify-center h-full">
         {/* Content will go here later */}
